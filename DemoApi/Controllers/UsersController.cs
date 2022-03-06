a1926de97a6751451ac5de7d9b491ba9497446d2
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-03-06 20:14:54 +0530

    Add project files.

---
+++ DemoApi/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DemoApi.Db;
+using DemoApi.Models;
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DemoDbContext _context;
+
+        public UsersController(DemoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.User.ToListAsync();
+        }
+       
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            user.Id=Guid.NewGuid();
+
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+       
+
+      
+    }
+}
---
+++ DemoApi/Db/DemoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DemoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoApi.Db
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserAddress> UserAddresses { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+        }
+    }
+}
